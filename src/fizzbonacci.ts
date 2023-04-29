74eb34708c84ff4ca6fbb7ea0fe83916b12d0bc2
Lachlan Hunt <lachlan.hunt@lachy.id.au>
2023-04-29 17:28:21 +1000

    Generate Fizzbonacci sequence with Amazon CodeWhisperer

--- src/fizzbonacci.ts
+++ src/fizzbonacci.ts
@@ -1,24 +1,19 @@
 // Create a generator function called "fizzbonacci" in TypeScript that accepts 2 parameters
 // as the first 2 numbers in Fibonacci sequence.
 // The output should follow the rules for FizzBuzz.
-// IF a value is divisible by 3, print "Fizz". IF a value is divisible by 5, print "Buzz".
-// IF a value is divisible by both 3 and 5, print "FizzBuzz".
+// If a value is divisible by 3, print "Fizz". IF a value is divisible by 5, print "Buzz".
+// If a value is divisible by both 3 and 5, print "FizzBuzz".
 // Create an array of length 20 populated with values from this generator.
 
 function* fizzbonacci(first: number, second: number) {
-    let [prev, curr] = [first, second];
-    while (true) {
-        [prev, curr] = [curr, prev + curr];
-        yield curr;
-    }
+  let firstNumber = first;
+  let secondNumber = second;
+  while (true) {
+    yield firstNumber;
+    firstNumber = secondNumber;
+    secondNumber = firstNumber + secondNumber;
+  }
 }
-
-const fb = fizzbonacci(1, 1);
-const arr = Array.from({ length: 20 }, () => {
-    const val = fb.next().value;
-    if (val % 3 === 0 && val % 5 === 0) return "FizzBuzz";
-    if (val % 3 === 0) return "Fizz";
-    if (val % 5 === 0) return "Buzz";
-    return val;
-});
-console.log(arr);
+const fizzbonacciGenerator = fizzbonacci(1, 1);
+const fizzbonacciArray = Array.from({ length: 20 }, () => fizzbonacciGenerator.next().value);
+console.log(fizzbonacciArray);
