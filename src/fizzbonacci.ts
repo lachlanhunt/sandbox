5959888a5924dd15b91aabbfa1eab51357f0a9c6
Lachlan Hunt <lachlan.hunt@lachy.id.au>
2023-04-30 19:49:07 +1000

    Generate Fizzbonacci sequence with TabNine Starter

--- src/fizzbonacci.ts
+++ src/fizzbonacci.ts
@@ -1,19 +1,21 @@
 // Create a generator function called "fizzbonacci" in TypeScript that accepts 2 parameters
 // as the first 2 numbers in Fibonacci sequence.
 // The output should follow the rules for FizzBuzz.
-// If a value is divisible by 3, print "Fizz". IF a value is divisible by 5, print "Buzz".
-// If a value is divisible by both 3 and 5, print "FizzBuzz".
 // Create an array of length 20 populated with values from this generator.
 
-function* fizzbonacci(first: number, second: number) {
-  let firstNumber = first;
-  let secondNumber = second;
-  while (true) {
-    yield firstNumber;
-    firstNumber = secondNumber;
-    secondNumber = firstNumber + secondNumber;
-  }
+function* fizzbonacci(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            yield "FizzBuzz";
+        } else if (i % 3 === 0) {
+            yield "Fizz";
+        } else if (i % 5 === 0) {
+            yield "Buzz";
+        } else {
+            yield i;
+        }
+    }
 }
-const fizzbonacciGenerator = fizzbonacci(1, 1);
-const fizzbonacciArray = Array.from({ length: 20 }, () => fizzbonacciGenerator.next().value);
-console.log(fizzbonacciArray);
+
+const fizzbonacciGenerator = fizzbonacci(20);
+console.log(fizzbonacciGenerator.next().value);
