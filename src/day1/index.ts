3f2bdfe1ced03c9f0dc7f86646671220bd333c3d
Lachlan Hunt <lachlan.hunt@lachy.id.au>
2023-12-12 11:24:15 +1100

    Day 1 part 2

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -4,16 +4,27 @@ import { readLines } from "../index";
 
 const lines = await readLines();
 
+const wordToDigit = (word: string) => {
+    if (word.match(/\d/)) {
+        return word;
+    }
+    return `${["ne", "wo", "hree", "four", "five", "six", "seven", "ight", "ine"].indexOf(word) + 1}`;
+};
+
 const mappedLines = lines.map((line) => {
-    // Search for the first digit in the line
-    const first = line.search(/\d/);
+    const matches = line.matchAll(/(?<=o)ne|(?<=t)wo|(?<=t)hree|four|five|six|seven|(?<=e)ight|(?<=n)ine|\d/g);
+    const allMatches = [...matches];
 
-    // Search for the last digit in the line
-    const last = line.search(/\d(?=\D*$)/);
+    const mappedAllMatches = allMatches.map((match) => wordToDigit(match[0]));
 
-    //    console.log(line, line[first], line[last]);
+    const firstMatch = mappedAllMatches.at(0) ?? "";
+    const lastMatch = mappedAllMatches.at(-1) ?? "";
 
-    return Number.parseInt(`${line[first]}${line[last]}`);
+    const result = Number.parseInt(`${firstMatch}${lastMatch}`);
+
+    // console.log(line, result);
+
+    return result;
 });
 
 const sum = mappedLines.reduce((acc, curr) => acc + curr, 0);
