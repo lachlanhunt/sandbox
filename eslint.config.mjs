dcf319d23bbd5bb281a9eda349b021340e51322e
Lachlan Hunt <lachlan.hunt@lachy.id.au>
2024-12-08 13:31:19 +1100

    Configure project

---
+++ eslint.config.mjs
@@ -0,0 +1,48 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint, { configs as tseslintConfigs } from "typescript-eslint";
+import importPlugin from "eslint-plugin-import";
+import { createTypeScriptImportResolver } from "eslint-import-resolver-typescript";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
+    {
+        languageOptions: {
+            globals: globals.node,
+            parserOptions: {
+                project: "./tsconfig.json",
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    pluginJs.configs.recommended,
+    // tseslint.configs.recommended,
+    tseslintConfigs.recommendedTypeChecked,
+    // tseslintConfigs.strict,
+    tseslintConfigs.strictTypeChecked,
+    // tseslintConfigs.stylistic,
+    tseslintConfigs.stylisticTypeChecked,
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+    importPlugin.flatConfigs.recommended,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+    importPlugin.flatConfigs.typescript,
+    {
+        settings: {
+            "import/parsers": {
+                "@typescript-eslint/parser": [".ts", ".tsx"],
+            },
+            "import/resolver": {
+                typescript: {},
+            },
+            "import/resolver-next": [
+                createTypeScriptImportResolver({
+                    alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+                }),
+            ],
+        },
+    },
+    eslintPluginPrettierRecommended,
+);
